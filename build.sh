15bda68684a1884384df201bec57d1785a42309e
Alexander Serebryakov <aserebryakov53@gmail.com>
2018-08-29 01:27:48 +0300

    ошибки

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ng build --prod --base-href "https://SerebryakovAlexander.github.io/MyWidget/ng build --prod --base-href "https://SerebryakovAlexander.github.io/MyWidget/""
+ng build --prod --base-href="https://serebryakovalexander.github.io/MyWidget/"
