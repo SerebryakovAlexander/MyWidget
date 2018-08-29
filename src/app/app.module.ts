70e771cf399178854c7140252c875ff16c57e558
Alexander Serebryakov <aserebryakov53@gmail.com>
2018-08-29 11:39:15 +0300

    верстка разбита на компоненты

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CopyRightComponent } from './copy-right/copy-right.component';
+import { HotelsComponent } from './hotels/hotels.component';
+import { WeatherComponent } from './weather/weather.component';
+import { TeddyBearComponent } from './teddy-bear/teddy-bear.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CopyRightComponent,
+    HotelsComponent,
+    WeatherComponent,
+    TeddyBearComponent
   ],
   imports: [
     BrowserModule
