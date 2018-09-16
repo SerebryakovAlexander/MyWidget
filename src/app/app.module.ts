e5f3c2fce703b276c761efab535f5c03309ecdd5
Alexander Serebryakov <aserebryakov53@gmail.com>
2018-09-16 09:53:10 +0300

    переместил компоненты

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { CopyRightComponent } from './copy-right/copy-right.component';
 import { HotelsComponent } from './hotels/hotels.component';
 import { WeatherComponent } from './weather/weather.component';
 import { TeddyBearComponent } from './teddy-bear/teddy-bear.component';
 import { HotelComponent } from './hotels/hotel/hotel.component';
-import { HotelFilterPipe } from './pipes/hotel-filter-pipe.pipe';
-import { PhoneFormatPipe } from './pipes/phone-format-pipe.pipe';
+import { HotelFilterPipe } from './hotels/pipes/hotel-filter-pipe.pipe';
+import { PhoneFormatPipe } from './hotels/pipes/phone-format-pipe.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    CopyRightComponent,
     HotelsComponent,
     WeatherComponent,
     TeddyBearComponent,
